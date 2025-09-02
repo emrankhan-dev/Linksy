c1bbff854353c02347d03ec109c6e4d4b894c840
Emran khan <emrankhan@Emrans-MacBook-Air.local>
2025-09-02 20:05:47 +0600

    first commit

---
+++ feedbacks.js
@@ -0,0 +1,44 @@
+const feedbacks = [
+    {
+        name: "Sarah K.",
+        message: "Linksy made it so easy to organize all my social links in one place. My followers love it!",
+        rating: 5
+    },
+    {
+        name: "John D.",
+        message: "I was able to create a professional page in minutes without any coding. Highly recommend!",
+        rating: 4
+    },
+    {
+        name: "Priya M.",
+        message: "The QR sharing feature is amazing. My clients love scanning and instantly accessing all my links.",
+        rating: 3
+    },
+]
+
+const feedbackListEl = document.getElementById('feedback-list')
+
+function fetchFeedbacks(reviews) {
+    reviews.forEach(review => {
+        const card = document.createElement('div')
+        card.classList = 'bg-gray-800 rounded-lg p-6 shadow-md hover:shadow-lg transition'
+
+        let stars = ''
+        for (let i = 1; i <= 5; i++) {
+            if (i <= review.rating) {
+                stars += '★'
+            } else {
+                stars += "☆"
+            }
+        }
+        card.innerHTML =
+            `
+        <div class="flex justify-center mb-2 text-yellow-400">${stars}</div>
+        <p class="text-gray-300 mb-4">"${review.message}"</p>
+        <h3 class="font-semibold text-white">– ${review.name}</h3>
+    `
+        feedbackListEl.append(card)
+    })
+}
+
+export {feedbacks, fetchFeedbacks}
---
+++ index.js
@@ -0,0 +1,9 @@
+import { feedbacks, fetchFeedbacks } from "./feedbacks.js"
+
+// fetch feedbacks dynamically
+fetchFeedbacks(feedbacks)
+
+// footer year changes dynamically
+const year = document.getElementById('year')
+const currentYear = new Date().getFullYear()
+year.textContent = currentYear
